5e89ddd9fedefe8d61be8bf8b4c7e96d625de11b
Brian Ujiie <bujiie+github@gmail.com>
2021-12-12 22:24:04 -0800

    Clean up code for day 13 solution.

--- day13/b.py
+++ day13/b.py
@@ -5,59 +5,45 @@ from sys import argv
 
 dots=set()
 folds=[]
-last_y_fold=None
-last_x_fold=None
 with open(argv[1]) as fp:
 	for i, l in enumerate(fp):
 		l=l.strip()
 		if l.startswith('fold'):
 			plane,line=l.split(' ')[-1].split('=')
-			line=int(line)
-			folds.append((plane, line))
-			if plane == 'y' and (last_y_fold is None or line < last_y_fold):
-				last_y_fold=line
-			if plane == 'x' and (last_x_fold is None or line < last_x_fold):
-				last_x_fold=line
-
+			folds.append((plane, int(line)))
 		elif len(l) == 0:
 			continue
 		else:
 			dots.add(tuple([int(n) for n in l.split(',')]))
 
-def fold_y(fold_on, dots=set()):
-	ylen=fold_on*2
-	new_dots=set()
+def fold(fold_on, dots=set()):
+	axis,fold_line=fold_on
+	# assumes we always fold exactly in half
+	b4_fold_len=fold_line*2
 
+	new_dots=set()
 	for dot in dots:
-		x,y=dot
-		if y > fold_on:
-			new_dots.add((x, ylen-y))
+		x,y = dot
+		if axis == 'x':
+			coord,folded_coord_pair=x,(b4_fold_len-x, y)
+		else:
+			coord,folded_coord_pair=y,(x, b4_fold_len-y)
+
+		if coord > fold_line:
+			new_dots.add(folded_coord_pair)
 		else:
 			new_dots.add(dot)
 	return new_dots	
 
-def fold_x(fold_on, dots=set()):
-	xlen=fold_on*2
-	new_dots=set()
-
-	for dot in dots:
-		x,y=dot
-		if x > fold_on:
-			new_dots.add((xlen-x, y))
-		else:
-			new_dots.add(dot)
-	return new_dots
-
 res=dots
-for fold in folds:
-	plane,line=fold
-	if plane == 'x':
-		res=fold_x(line, res)
-	else:
-		res=fold_y(line, res)
+last_folds={}
+for f in folds:
+	axis,fold_line=f
+	last_folds[axis]=fold_line
+	res=fold(f, res)
 
-for y in range(last_y_fold):
-	for x in range(last_x_fold):
+for y in range(last_folds['y']):
+	for x in range(last_folds['x']):
 		if (x,y) in res:
 			print('#', end='')
 		else:
